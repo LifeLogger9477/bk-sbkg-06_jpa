16b10d8f7fb15387eca370fd774f4878a90984b5
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-04 17:09:30 +0900

    스프링부트 핵심 가이드 06장_데이터베이스 연동 : ProductDTO + ProductResponseDTO 생성 - builder 메소드로 변경 전

---
+++ src/main/java/me/sbkg/jpa/data/service/ProductService.java
@@ -0,0 +1,8 @@
+package me.sbkg.jpa.data.service;
+
+/**
+ * author : ms.Lee
+ * date   : 2024-01-04
+ */
+public interface ProductService {
+}
---
+++ src/main/java/me/sbkg/jpa/data/service/impl/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package me.sbkg.jpa.data.service.impl;
+
+/**
+ * author : ms.Lee
+ * date   : 2024-01-04
+ */
+public class ProductServiceImpl {
+}
